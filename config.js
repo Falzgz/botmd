1c4c6469e9fdb57e381e986b931d1bdde0674ee9
Falzgz <rifalzstore14@gmail.com>
2022-08-19 00:12:16 +0700

    botmd

---
+++ config.js
@@ -0,0 +1,106 @@
+let fs = require('fs')
+global.owner = JSON.parse(fs.readFileSync('./src/owner.json')) // Put your number to folder /src/owner.json
+global.mods = JSON.parse(fs.readFileSync('./src/moderator.json')) // Want some help?
+
+global.APIs = { // API Prefix
+  // name: 'https://website'
+  nrtm: 'https://nurutomo.herokuapp.com',
+  bg: 'http://bochil.ddns.net',
+  xteam: 'https://api.xteam.xyz',
+  zahir: 'https://zahirr-web.herokuapp.com',
+  zeks: 'https://api.zeks.me',
+  pencarikode: 'https://pencarikode.xyz',
+  LeysCoder: 'https://leyscoders-api.herokuapp.com',
+  neoxr: 'https://neoxr-api.herokuapp.com',
+  amel: 'https://melcanz.com',
+  hardianto: 'https://hardianto.xyz',
+  lol: 'https://api.lolhuman.xyz',
+  adicug: 'https://api.adiofficial.xyz'
+}
+global.APIKeys = { // APIKey Here
+  // 'https://website': 'apikey'
+  'https://neoxr-api.herokuapp.com': 'yntkts',
+  'https://api.xteam.xyz': 'apikeylu',
+  'https://melcanz.com': 'apikeylu',
+  'https://api.lolhuman.xyz': 'apikeylu',
+  'https://zahirr-web.herokuapp.com': 'zahirgans',
+  'https://api.zeks.me': 'apikeylu',
+  'https://pencarikode.xyz': 'apikeylu',
+  'https://hardianto.xyz': 'hardianto',
+  'https://leyscoders-api.herokuapp.com': 'apikeylu',
+  'https://api.adiofficial.xyz': 'apikey lu'
+}
+
+//global.wm = '𝑨𝒓𝒊𝒆𝑻𝒖𝒃𝒆 𝑩𝒐𝒕'
+global.wait = '_*𝒔𝒆𝒅𝒂𝒏𝒈 𝒅𝒊 𝒑𝒓𝒐𝒔𝒆𝒔...*_'
+global.eror = '_*𝒎𝒂𝒂𝒇 𝒔𝒆𝒓𝒗𝒆𝒓 𝒆𝒓𝒓𝒐𝒓...*_'
+
+//========Url Template Buttons=========//
+global.dtu = 'Group bot'
+global.urlnya = "https://chat.whatsapp.com/Dj8kMyI48u6KGbaAQtKi70"
+
+//============= callButtons =============//
+global.dtc = '𝒄𝒂𝒍𝒍 𝒐𝒘𝒏𝒆𝒓'
+global.phn = '3'
+
+//============= Games ================//
+global.benar = '_*Benar*_✅'
+global.salah = '_*Salah*_❌'
+global.dikit = "dikit lagi"
+
+global.multiplier = 100 // The higher, The harder levelup
+
+//=========== Requirements ==========//
+
+global.baileys = require('@adiwajshing/baileys')
+global.fs = require('fs')
+global.data = JSON.parse(fs.readFileSync('./data.json'))
+global.fetch = require('node-fetch')
+global.bochil = require('@bochilteam/scraper')
+
+global.rpg = {
+  emoticon(string) {
+    string = string.toLowerCase()
+    let emot = {
+      level: '🧬',
+      limit: '🌌',
+      healt: '❤️',
+      exp: '✉️',
+      money: '💵',
+      potion: '🥤',
+      diamond: '💎',
+      common: '📦',
+      uncommon: '🎁',
+      mythic: '🗳️',
+      legendary: '🗃️',
+      pet: '🎁',
+      sampah: '🗑',
+      armor: '🥼',
+      fishingrod: '🎣',
+      pickaxe: '⛏️',
+      sword: '⚔️',
+      kayu: '🪵',
+      batu: '🪨',
+      iron: '⛓️',
+      string: '🕸️',
+      kuda: '🐎',
+      kucing: '🐈' ,
+      anjing: '🐕',
+      makananpet: '🍖',
+      gold: '👑',
+      emerald: '💚'
+    }
+    let results = Object.keys(emot).map(v => [v, new RegExp(v, 'gi')]).filter(v => v[1].test(string))
+    if (!results.length) return ''
+    else return emot[results[0][0]]
+  }
+}
+
+let chalk = require('chalk')
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+  fs.unwatchFile(file)
+  console.log(chalk.redBright("Update 'config.js'"))
+  delete require.cache[file]
+  require(file)
+})
---
+++ plugins/donasi.js
@@ -0,0 +1,60 @@
+const { default: makeWASocket, BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, downloadContentFromMessage, downloadHistory, proto, getMessage, generateWAMessageContent, prepareWAMessageMedia } = require('@adiwajshing/baileys')
+let handler = async (m) => {
+let duit = `*────── 「 DONATE 」 ──────*
+
+Hai 👋
+Kalian bisa mendukung saya agar bot ini tetap up to date dengan:
+
+┌〔 Donasi • Emoney 〕
+├🔃 Gopay : 082113034413
+├🔃 Dana  : 082113034413
+└────
+
+Berapapun donasi kalian akan sangat berarti 👍
+𝐀𝐫𝐢𝐠𝐚𝐭𝐨𝐮!!!
+
+Contact person Owner:
+wa.me/6289518022967 (Owner)`
+let message = await prepareWAMessageMedia({ image: {url: 'https://telegra.ph/file/696076406bf2516adb1fd.jpg' }}, { upload: conn.waUploadToServer })
+     const template = generateWAMessageFromContent(m.chat, proto.Message.fromObject({
+     templateMessage: {
+         hydratedTemplate: {
+           imageMessage: message.imageMessage,
+           hydratedContentText: duit,
+           hydratedFooterText: wm,
+           hydratedButtons: [{
+             urlButton: {
+               displayText: '🏧 Sociabuzz',
+               url: 'https://sociabuzz.com/arietube/tribe'
+             }
+
+           },
+               {
+             callButton: {
+               displayText: 'Gopay',
+               phoneNumber: '+62 821-1303-4413'
+             }
+           },           
+               {
+             quickReplyButton: {
+               displayText: '🧒 Owner',
+               id: '.owner',
+             }
+
+           }]
+         }
+       }
+     }), { userJid: m.sender, quoted: m });
+    //conn.reply(m.chat, text.trim(), m)
+    return await conn.relayMessage(
+         m.chat,
+         template.message,
+         { messageId: template.key.id }
+     )
+}
+
+handler.help = ['donasi']
+handler.tags = ['info']
+handler.command = /^dona(te|si)|bagiduit$/i
+
+module.exports = handler
